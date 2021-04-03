a79c932c9126431f947c58b9a5c8a2a417c679bc
László Ádám <laszloadam08@gmail.com>
2021-04-03 10:01:59 +0200

    update-2021-04-03

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
 console.log('Hello World!');
 let makeDiv = document.getElementById('dot');
-  makeDiv.innerText = '*';
-  let randomNumber = Math.floor(Math.random()*100);
+  let randomWay = Math.floor(Math.random()*100);
+  let randomPosition = Math.floor(Math.random()*100);
   let randomSize = Math.floor(Math.random()*100)+20;
   let randomOpacity = Math.floor(Math.random()*10);
   
-  makeDiv.style.fontSize = randomSize+'%';
-  makeDiv.style.left = randomNumber+'vw';
+  makeDiv.style.zoom = randomSize+'%';
+  makeDiv.style.left = randomWay+'vw';
+  makeDiv.style.left = randomPosition+'vw';
   makeDiv.style.opacity = '0.'+randomOpacity;
 
-function moveDiv(){
-  makeDiv.style.transform ='translate(200px 200px)' 
-}
